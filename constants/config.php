cf4d3fa8d8978d71c5d37ea66a1e07eaddc333e2
Tajwar Tajim <38377487+TajwarTajim@users.noreply.github.com>
2021-05-05 03:31:39 +0600

    Create config.php

---
+++ constants/config.php
@@ -0,0 +1,38 @@
+<?php
+//Website Settings
+$site_title = "<h3>Bharamara</h3>";
+$about_site = "A free classified ads website for Cars, Phones, Electronics, Smartphones, Real estate, and everything else. Find what you're looking for or create your own ad for free!.";
+$site_titlex = "Bharamara";
+date_default_timezone_set('GMT');
+
+$currency_before = "Tk";
+$currency = "/=";
+$installation_path = "localhost/bharamra-ud";
+
+
+//Social Network Setings
+$facebook_link = "#";
+$twitter_link = "#";
+$instagram_link = "#";
+$googleplus_link = "#";
+
+//Database Settings
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bharamara";
+
+//Contact Settings
+$site_email = "bharamara.official@gmail.com";
+$site_phone = "+088 01739415607";
+$site_address = "Demo Address from bangladesh";
+
+//SMTP Settings
+$smtp_host = 'mail.bharamara.com';
+$smtp_user = 'developer@bharamara.com';
+$smtp_pass = 'eN4%hV1~iP4)oW5';
+
+
+
+
+?>
