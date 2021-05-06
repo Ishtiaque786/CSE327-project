b0392f082b4fd9e982cb177bd7d1eb57c615db49
Shuvo1620 <80590959+Shuvo1620@users.noreply.github.com>
2021-05-06 20:05:10 +0600

    Add files via upload

--- about.php
+++ about.php
@@ -1,97 +1,200 @@
 <?php
-  require 'constants/config.php';
-  require 'constants/check-login.php';
-  $page = "about";
-  ?>
+require '../constants/config.php';
+require '../constants/check-login.php';
+
+if ($logged == "1") {
+       if ($myrole == "admin") {
+
+       }else{
+
+        header("location:../");
+
+       }
+
+    }else{
+
+        header("location:../");
+
+    }
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title><?php echo $site_titlex; ?> - About Us</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/fonts/line-icons.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/slicknav.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/color-switcher.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
-    <link rel="icon" href="assets/icon/favicon.ico">
-  </head>
-  <body>
-    <header id="header-wrap">
-		<?php include("./Snippets/top-nav.php");?>
-    </header>
-    <div class="page-header" style="background: url(assets/img/banner1.jpg);">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="breadcrumb-wrapper">
-              <h2 class="product-title">About Us</h2>
-              <ol class="breadcrumb">
-                <li><a href="./">Home /</a></li>
-                <li style="color:white;" class="current">About Us</li>
-              </ol>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?php echo $site_titlex; ?> - About</title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="icon" href="../assets/icon/favicon.ico">
+        <link rel="stylesheet" href="summernote/dist/summernote.css" />
+    <link rel="stylesheet" href="summernote/dist/summernote-bs3.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/main-admin.css">
+
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a target="_blank" class="navbar-brand" href="../"><?php echo $site_title; ?></a>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <section id="about" class="section-padding">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 col-lg-12 col-xs-12">
-            <div class="about-wrapper">
-              <p class="intro-desc">
-                <?php
-                  try {
-                     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                  
-                  
-                  $stmt = $conn->prepare("SELECT * FROM tbl_about LIMIT 1");
-                  $stmt->execute();
-                  $result = $stmt->fetchAll();
-                  
-                     foreach($result as $row)
-                     {
-                  
-                  echo $row['about'];
-                  
-                  
-                  }
-                  				  
-                  }catch(PDOException $e)
-                     {
-                     echo "Connection failed: " . $e->getMessage();
-                     }
-                  
-                     ?>
-              </p>
+        
+
+            <ul class="nav navbar-top-links navbar-right">
+
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i> <?php echo $myusername; ?> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="account"><i class="fa fa-gear fa-fw"></i> Account Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="../logout"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+
+                </li>
+
+            </ul>
+           
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+
+                        <li>
+                            <a href="./"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="pub-ads"><i class="fa fa-edit fa-fw"></i> Published Ads</a>
+                        </li>
+                        <li>
+                            <a href="active-ads"><i class="fa fa-check-circle-o fa-fw"></i> Active Ads</a>
+                        </li>
+                        <li>
+                            <a href="pending-ads"><i class="fa fa-spinner fa-fw"></i> Pending Ads</a>
+                        </li>
+                        <li>
+                            <a href="featured-ads"><i class="fa fa-star-o fa-fw"></i> Featured Ads</a>
+                        </li>
+                        <li>
+                            <a href="users"><i class="fa fa-users fa-fw"></i> Users</a>
+                        </li>
+                        <li>
+                            <a href="account"><i class="fa fa-cog fa-fw"></i> Account Settings</a>
+                        </li>
+                        <li>
+                            <a href="faq"><i class="fa fa-question-circle fa-fw"></i> FAQ</a>
+                        </li>
+                        <li>
+                            <a href="about"><i class="fa fa-info-circle fa-fw"></i> About</a>
+                        </li>
+                         <li>
+                            <a href="../logout"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+
+                    </ul>
+                </div>
+              
             </div>
-          </div>
+        
+        </nav>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h4 class="page-header">About</h4>
+                    <?php require 'constants/check-reply.php'; ?>
+                    <form action="app/update-about.php" method="POST" >
+                    <textarea id="summernote" name="about" required>                        <?php
+
+                        try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    
+$stmt = $conn->prepare("SELECT * FROM tbl_about LIMIT 1");
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+    foreach($result as $row)
+    {
+        
+    echo $row['about'];
+
+    }
+                      
+    }catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+
+                        ?>
+
+                    </textarea>
+                    <button type="submit" class="btn btn-default">Save</button><br><br>
+                </form>
+                </div>
+
+
+            </div>
+         
+            <div class="row">
+
+            </div>
+
         </div>
-      </div>
-    </section>
-    <?php include("./Snippets/footer.php");?>
-    <a href="#" class="back-to-top">
-    <i class="lni-chevron-up"></i>
-    </a>
-    <div id="preloader">
-      <div class="loader" id="loader-1"></div>
+
     </div>
-    <script src="assets/js/jquery-min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/jquery.counterup.min.js"></script>
-    <script src="assets/js/waypoints.min.js"></script>
-    <script src="assets/js/wow.js"></script>
-    <script src="assets/js/owl.carousel.min.js"></script>
-    <script src="assets/js/jquery.slicknav.js"></script>
-    <script src="assets/js/main.js"></script>
-    <script src="assets/js/form-validator.min.js"></script>
-    <script src="assets/js/contact-form-script.min.js"></script>
-    <script src="assets/js/summernote.js"></script>
-  </body>
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="vendor/metisMenu/metisMenu.min.js"></script>
+    <script src="dist/js/sb-admin-2.js"></script>
+    <script src="summernote/dist/summernote.min.js"></script>
+    
+<script>
+    $('#summernote').summernote();
+
+    $(function () {
+
+        $('.summernote').summernote();
+
+        $('.summernote1').summernote({
+            toolbar: [
+                ['headline', ['style']],
+                ['style', ['bold', 'italic', 'underline', 'superscript', 'subscript', 'strikethrough', 'clear']],
+                ['textsize', ['fontsize']],
+                ['alignment', ['ul', 'ol', 'paragraph', 'lineheight']],
+            ]
+        });
+
+        $('.summernote2').summernote({
+            airMode: true,
+        });
+
+    });
+
+</script>
+
+</body>
+
 </html>
